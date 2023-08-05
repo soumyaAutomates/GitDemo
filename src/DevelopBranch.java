bebdc849149a10f3d6da1721de2c32e6ae720786
soumya <soumya4193@gmail.com>
2023-08-05 18:53:37 +0530

    Develop branch from GitDemo folder

---
+++ src/DevelopBranch.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DevelopBranch {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		
+		
+		System.out.println("This is from GitDemo");
+		System.out.println("This is from GitHub folder 1");
+		System.out.println("This is from GitHub folder 1");
+		System.out.println("This is from GitHub folder 1");
+		
+		
+
+	}
+
+}
